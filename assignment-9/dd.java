3c812cc84bf1f2c81c2267f1cb16f18f31aa2847
D2_suyash_84016 <patilsuyash1177@gmail.com>
2024-05-01 16:30:08 +0530

    assignment-9  completed

---
+++ assignment-9/dd.java
@@ -0,0 +1,19 @@
+package assignment9;
+
+import java.util.Scanner;
+
+public class dd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		Scanner sc =new Scanner(System.in);
+//		que2 q2=new que2();
+//		System.out.println("enter new string");
+//		String s1=sc.next();
+//		q2.checkpalindrome(s1);
+		Q3 e=new Q3();
+		e.count();
+	}
+
+}
